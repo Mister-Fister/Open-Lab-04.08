bbbf6790fb082c4f0b7a3a550208f4920ac14c14
Matúš <matusskor@gmail.com>
2020-10-30 11:31:47 +0100

    zmeny

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Open_Lab_04._08
 {
@@ -6,7 +7,16 @@ namespace Open_Lab_04._08
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            List<string> a = new List<string>();
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].Length == 4)
+                {
+                    a.Add(strings[i]);
+                }
+            }
+            return a.ToArray();
+
         }
     }
 }
